ef631d2040f4ec4212eaa31bb4449c229241abf2
abhi <abhijithg@gmail.com>
2011-02-22 12:28:10 +0530

    * progress bar code cleaned up * TODO: generalize so that backend logic can be passed in as fn or a macro

--- src/cxr/swing/dialog.clj
+++ src/cxr/swing/dialog.clj
@@ -6,14 +6,14 @@
   (:use (clojure.contrib
          [miglayout :only (miglayout components)]
          [swing-utils :only (make-menubar add-action-listener)]))
-    (:require [cxr.globals :as globals]))
+  (:require [cxr.globals :as globals]))
 
 
 (def msg-type { :info    JOptionPane/INFORMATION_MESSAGE
-		:error   JOptionPane/ERROR_MESSAGE
-		:warn    JOptionPane/WARNING_MESSAGE
-		:message JOptionPane/PLAIN_MESSAGE
-		})
+               :error   JOptionPane/ERROR_MESSAGE
+               :warn    JOptionPane/WARNING_MESSAGE
+               :message JOptionPane/PLAIN_MESSAGE
+               })
 
 (defn- dialog
   [parent msg type]
@@ -30,44 +30,46 @@
 ;; progress bar code
 (def pb (JProgressBar. 0 10))
 (def pb-agent (agent 0)) ;; update progress of finding files for indexing using the progress as the value of this agent
-(def finder (agent nil)) ;; index menu action dispatched to this agent
+(def determinate (agent nil))
 
-(defn move
-  [x lst i end]
-  (if (and (deref globals/running) (not (= i end)))
-    (do (send *agent* move (rest lst) (inc i) end)
-      ;; essence can be pulled out of this function; apply f args
+(defn task
+  [x lst start end]
+  (if (and (deref globals/running) (not (= start end)))
+    (do (send *agent* task (rest lst) (inc start) end)
+      ;; essence can be pulled out of this function; apply f args or a macro (would work out better?)
         (let [fname (first lst)] ;; BACKEND functionality
-          (Thread/sleep 1)
-          (debug (str fname "indexed"))
+          (Thread/sleep 1000)
+          (debug (str fname " " "indexed"))
       (inc x)))
     (do (reset! globals/running false) end)))
 
-(defn init-finder-agent-watch [] ;; change the mode of the progress bar from indeterminate to determinate and set the start and end values after find-files has finished
-  (add-watch finder
-    :finder
+;; change the mode of the progress bar from indeterminate to determinate and set the start and end values after find-files has finished
+(defn init-determinate-agent-watch
+  [] 
+  (add-watch determinate
+    :determinate
     (fn [k r o n]
-      (let [ lst (range 0 10) end 10 ] ;; BACKEND functionality. TODO: get files that are not indexed ONLY
+      (let [ lst (deref *agent*) end (count lst) ]
 	(.setIndeterminate pb false)
-	(.setMaximum pb end) ;; determine total num of files and set progress bar length (start & end)
-	(send pb-agent move lst 0 end)))))
+	(.setMaximum pb end) 
+	(send pb-agent task lst 0 end)))))
 
 (defn init-pb-agent-watch [] ;; set the pb-agent's value as the progress value when the value of pb-agent changes
   (add-watch pb-agent :pb-agent
     (fn [k r o n]
       (.setValue pb n) pb)))
 
-;; progressbar
-;; - set determinate mode or set start end
-;; - set progress value
-;; - fn to set start-end (switch to determinate mode)
-;; - fn & args to run and whose progress is to be monitored
+;;; progressbar
+;;; - set determinate mode or set start end
+;;; - set progress value
+;;; - fn to set start-end (switch to determinate mode)
+;;; - fn & args to run and whose progress is to be monitored
 
 (defn progress-bar
-  [frame f & args]
+  [frame]
   (let [ panel (miglayout (JPanel.)
-		 (JButton. "stop") { :id :stop })
-	 dialog (JDialog. frame) ]
+                          (JButton. "stop") { :id :stop })
+        dialog (JDialog. frame) ]
     (.add panel pb)
     (.setIndeterminate pb true)
     (.setString pb "0")
@@ -77,11 +79,10 @@
       (.setModal false)
       (.pack)
       (.setVisible true))
-    (dosync (reset! globals/running true)) 
-    (add-action-listener (:stop (components panel)) (fn [_] (dosync (reset! globals/running false))))
-    (init-finder-agent-watch)
+    (dosync (reset! globals/running true))
+    (init-determinate-agent-watch)
     (init-pb-agent-watch)
-    (apply f args)))
+    (add-action-listener (:stop (components panel)) (fn [_] (dosync (reset! globals/running false))))))
 
 (defn ask-open
   [event frame]
@@ -90,8 +91,8 @@
     (let [ ret (.showOpenDialog chooser frame)]
       (.setFileSelectionMode chooser JFileChooser/DIRECTORIES_ONLY)
       (cond
-       (= JFileChooser/APPROVE_OPTION ret) (do
-                                             (debug (.getSelectedFile chooser))
-                                             (progress-bar frame send-off finder (fn [_] (range 0 10)))) ;; BACKEND functionality.
-	(= JFileChooser/CANCEL_OPTION ret) (debug "canceleshwar")
-	:else "error"))))
+       (= JFileChooser/APPROVE_OPTION ret) (let [pb (progress-bar frame) dir (.getSelectedFile chooser)]
+                                             (debug dir)
+                                             (send determinate (fn [_] (file-seq (clojure.java.io/as-file dir))))) ;; backend
+       (= JFileChooser/CANCEL_OPTION ret) (debug "canceleshwar")
+       :else "error"))))
