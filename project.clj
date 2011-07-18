d964b22a2a72b8d42625fccd1611e0106949afe3
abhi <abhijithg@gmail.com>
2011-07-18 21:59:26 +0530

    log4j added as dependency. commons logging is pulled with it

--- project.clj
+++ project.clj
@@ -5,9 +5,11 @@
                  [com.h2database/h2 "1.2.140"]
                  [jmimemagic/jmimemagic "0.1.2"]
                  [org/apache/pdfbox/pdfbox "1.2.1"]
-                 [jmimemagic/jmimemagic "0.1.2"]
-                 [commons-logging/commons-logging "1.1.1"]
                  [oro/oro "2.0.8"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
                  [com/miglayout/miglayout "3.7.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :aot [cxr.db.tables cxr.swing.core]
